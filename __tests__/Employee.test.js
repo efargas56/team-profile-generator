49be32156b233bd2c123a00e0b8174b4c0b45b30
ef56 <efargas56@gmail.com>
2022-07-13 18:23:37 -0400

    created tests for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,24 @@
 const Employee = require('../lib/Employee');
 
 test('Created an employee class', () => {
-    const employee = new Employee('jared', 'manager')
+    const employee = new Employee('jared', 36, 'Jared@yahoo.com')
 
     expect(employee.name).toBe(String)
     expect(employee.id).toBe(String)
     expect(employee.email).toBe(String)
 });
-test('checks to see if if you pulled the ID', () => {
-    const employee = new Employee('jared', 'manager')
+test('get employees name', () => {
+    const employee = new Employee('jared', 36, 'Jared@yahoo.com')
+    expect(employee.getName().toEqual(expect.any(String)))
 
-}
+})
+test('get employee ID', () => {
+    const employee = new Employee('jared', 36, 'Jared@yahoo.com')
+    expect(employee.getId().toEqual(expect.any(Number)))
+
+})
+test('get employee email', () => {
+    const employee = new Employee('jared', 36, 'Jared@yahoo.com')
+    expect(employee.getEmail().toequal(expect.any(String)))
+
+})
